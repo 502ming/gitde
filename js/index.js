47bc34feb89118c9ac8d0647acfa95a823fdcdc4
keke666 <502976367@qq.com>
2022-11-13 04:00:32 +0800

    添加功能完成

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ const {data}=await axios.get('/dashboard')
  initSalaryChart(data.salaryData)
  initGroupChart(data.groupData)
  initGenderChart(data.salaryData)
+ initMapChart(data.provinceData)
 })
 
 
@@ -233,3 +234,133 @@ function initGenderChart(salaryData){
     myChart.setOption(option)
   
 }
+
+
+
+
+
+
+
+const initMapChart = (provinceData) => {
+  const myEchart = echarts.init(document.querySelector('#map'))
+  console.log(provinceData)
+  // 默认数据项
+  const dataList = [
+    { name: '南海诸岛', value: 0 },
+    { name: '北京', value: 0 },
+    { name: '天津', value: 0 },
+    { name: '上海', value: 0 },
+    { name: '重庆', value: 0 },
+    { name: '河北', value: 0 },
+    { name: '河南', value: 0 },
+    { name: '云南', value: 0 },
+    { name: '辽宁', value: 0 },
+    { name: '黑龙江', value: 0 },
+    { name: '湖南', value: 0 },
+    { name: '安徽', value: 0 },
+    { name: '山东', value: 0 },
+    { name: '新疆', value: 0 },
+    { name: '江苏', value: 0 },
+    { name: '浙江', value: 0 },
+    { name: '江西', value: 0 },
+    { name: '湖北', value: 0 },
+    { name: '广西', value: 0 },
+    { name: '甘肃', value: 0 },
+    { name: '山西', value: 0 },
+    { name: '内蒙古', value: 0 },
+    { name: '陕西', value: 0 },
+    { name: '吉林', value: 0 },
+    { name: '福建', value: 0 },
+    { name: '贵州', value: 0 },
+    { name: '广东', value: 0 },
+    { name: '青海', value: 0 },
+    { name: '西藏', value: 0 },
+    { name: '四川', value: 0 },
+    { name: '宁夏', value: 0 },
+    { name: '海南', value: 0 },
+    { name: '台湾', value: 0 },
+    { name: '香港', value: 0 },
+    { name: '澳门', value: 0 },
+  ]
+  // 需要将后台返回的数据项，匹配设置给默认的数据项
+  // 遍历dataList, 依次在后台返回的数据中找，看有没有相同name的项，如果有，更新一下value
+  dataList.forEach(item => {
+    // 尝试拿item.name去provinceData数组中找对应项
+    const obj = provinceData.find(v => {
+      const regExp = /省|回族自治区|维吾尔自治区|壮族自治区|特别行政区|自治区/g
+      return v.name.replace(regExp, '') === item.name
+    })
+    // 找到了，说明有这个省份的同学，更新
+    if (obj) {
+      item.value = obj.value
+    }
+  })
+
+  let option = {
+    title: {
+      text: '籍贯分布',
+      top: 20,
+      left: 10,
+      textStyle: {
+        fontSize: 16,
+      },
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{b}: {c} 位学员',
+      borderColor: 'transparent',
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      textStyle: {
+        color: '#fff',
+      },
+    },
+    visualMap: {
+      min: 0,
+      max: 6,
+      left: 'left',
+      bottom: '20',
+      text: ['6', '0'],
+      inRange: {
+        color: ['#ffffff', '#0075F0'],
+      },
+      show: true,
+      left: 40,
+    },
+    geo: {
+      map: 'china',
+      roam: false,
+      zoom: 1.0,
+      label: {
+        normal: {
+          show: true,
+          fontSize: '10',
+          color: 'rgba(0,0,0,0.7)',
+        },
+      },
+      itemStyle: {
+        normal: {
+          borderColor: 'rgba(0, 0, 0, 0.2)',
+          color: '#e0ffff',
+        },
+        emphasis: {
+          areaColor: '#34D39A',
+          shadowOffsetX: 0,
+          shadowOffsetY: 0,
+          shadowBlur: 20,
+          borderWidth: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)',
+        },
+      },
+    },
+    series: [
+      {
+        name: '籍贯分布',
+        type: 'map',
+        geoIndex: 0,
+        data: dataList,
+      },
+    ],
+  }
+  myEchart.setOption(option)
+}
+
